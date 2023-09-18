41d33d05a5207d67f0394a7edf2f5abd2d545ea7
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2023-09-17 20:40:28 -0500

    Test

---
+++ src/components/pages/Login.tsx
@@ -0,0 +1,43 @@
+import NavBar from "../organs/NavBar";
+import Footer from "../organs/Footer";
+import "./Login.css"; // Archivo CSS para el estilo específico de Login
+import { useState } from "react";
+import { useAuth } from "../../Auth/AuthProvider";
+import { Navigate } from "react-router-dom";
+import bgImage from "../../assets/HeroVector.png"
+import { Image } from "../atoms/Image"
+
+export default function Login() {
+
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+  const auth = useAuth();
+
+  if(auth.isAuthenticated){
+    return <Navigate to="/Home" />
+  }
+
+  return (
+    <div className="login-container">
+      <NavBar />
+      <section className="login-section">
+        {/* <Image className="h-[60%] w-[80%] lg:h-[90vh] md:h-[50vh] lg:w-1/2 md:w-[55%]" image={bgImage} alt="Hero Background Vector" /> */}
+        <div className="login-content">
+          <h1 className="login-title">Login</h1>
+          <form className="login-form">
+            <div className="form-group">
+              <label>Email</label>
+              <input type="text" value={email} onChange={(e) => setEmail (e.target.value )} className="login-input" />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input type="password" value={password} onChange={(e) => setPassword (e.target.value)} className="login-input" />
+            </div>
+            <button className="login-button">Login</button>
+          </form>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from './components/pages/Login.tsx';
+import { AuthProvider } from './Auth/AuthProvider.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/Login",
+    element: <Login />
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>,
+)
