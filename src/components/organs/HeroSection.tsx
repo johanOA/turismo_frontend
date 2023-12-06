8340c049fb8e81c49609104dc77b900e50174ae0
eltin28 <juanj.herrerao@uqvirtual.edu.co>
2023-12-06 10:32:35 -0500

    Actualizacion login

--- src/components/organs/HeroSection.tsx
+++ src/components/organs/HeroSection.tsx
@@ -9,44 +9,12 @@
 import "../Styles/PrincipalPage.css";
 
 const HeroSection = () => {
-<<<<<<< HEAD
-    return (
-        <section className="w-full lg:h-screen md:h-[550px] h-[830px] relative overflow-x-hidden flex justify-end bg-#02590F">
-            <Image className="h-[60%] w-[80%] lg:h-[90vh] md:h-[50vh] lg:w-1/2 md:w-[55%]" image={bgImage} alt="Hero Background Vector" />
-            <main className="w-full lg:h-full h-auto grid md:grid-cols-2 absolute top-0 left-0 lg:px-24 md:px-8 px-5 pt-24 md:pt-32 lg:pt-0">
-                <div className="flex flex-col justify-center md:gap-6 gap-3 md:order-1 order-2">
-                    <Text as="p" className="text-color1 uppercase tracking-widest lg:text-base  text-sm font-normal">
-                        <Fade>{HeroTexts.firstText}</Fade>
-                    </Text>
-                    <Text as="h1" className=" text-color3 lg:text-7xl md:text-5xl text-3xl font-medium">
-                        <Fade>{HeroTexts.secondText}</Fade>
-                    </Text>
-                    <Text as="p" className="text-color3 md:text-base text-sm text-justify font-light">
-                        <Fade>{HeroTexts.thirdText}</Fade>
-                    </Text>
-                    <div className="w-full flex md:justify-start justify-between items-center lg:gap-12 md:gap-6 gap-0">
-                        <Button type="button" className="outline-none border-none lg:px-7 px-5 py-3 bg-color2 text-white font-extralight rounded-lg">
-                            {HeroTexts.firstButton}
-                        </Button>
-                        <div className="flex items-center lg:gap-6 gap-3 cursor-pointer">
-                            <Text as="span" className="relative flex h-14 w-14">
-                                <Text as="span" className="animate-ping absolute inline-flex h-full w-full rounded-full bg-color1 opacity-75"></Text>
-                                <Text as="span" className="relative flex justify-center items-center text-white rounded-full h-14 w-14 bg-color1">
-                                    <Play size={20} color="currentColor" weight="fill" />
-                                </Text>
-                            </Text>
-                            <Button type="button" className="outline-none border-none">
-                                {HeroTexts.secondButton}
-                            </Button>
-                        </div>
-=======
         
     const nextDom: HTMLElement | null = document.getElementById('next-hs');
     const prevDom: HTMLElement | null = document.getElementById('prev-hs');
     const carouselDom: HTMLElement | null = document.querySelector('.carousel-hs');
     const listItemDom: HTMLElement | null = document.querySelector('.carousel-hs .list-hs');
     const thumbnailDom: HTMLElement | null = document.querySelector('.carousel-hs .thumbnail-hs');
->>>>>>> a3a00c07dab9d1532881e6673bb35e79587a7c4c
 
     if (nextDom) {
         nextDom.onclick = function () {
