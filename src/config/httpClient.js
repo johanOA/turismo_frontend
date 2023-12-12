3701956e3d0e383b63673c49c2e61af2f127d3c7
optitudes <sebasacuasssssrio@gmail.com>
2023-12-12 13:03:28 -0500

    updated httpClient

--- src/config/httpClient.js
+++ src/config/httpClient.js
@@ -1,7 +1,36 @@
 import axios from "axios";
+import { clearLocalStorage,getUserToken } from "../localStorage/localStorage";
 
 const instance = axios.create({
-    baseURL: 'https://beta.api.turismoenlacordillera.com/api/',
-    timeout: 30000,
+   baseURL: 'https://beta.api.turismoenlacordillera.com/api/',
+   timeout: 30000,
 });
+
+instance.interceptors.request.use(
+ function(config) {
+  const token = getUserToken(); 
+  if (token) {
+    config.headers["Authorization"] = token;
+  }
+  return config;
+ },
+ function(error) {
+  return Promise.reject(error);
+ }
+);
+
+instance.interceptors.response.use(
+    function(response) {
+      return response;
+    },
+    function(error) {
+
+      if (error.response.status === 401) {
+        clearLocalStorage();
+        // Ejecutar la función de redirección cuando ocurra un error 401
+        window.location.href = "/login";
+      }
+      return Promise.reject(error);
+    }
+  );
 export default instance;
